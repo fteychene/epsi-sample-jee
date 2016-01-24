641a6970713d6efd9fbbf2be62f2e719129afd41
fteychene <francois.teychene@gmail.com>
2016-01-24 22:57:04 +0100

    Add JPA sample.

--- src/main/java/fte/tests/jeerestcdi/rest/GreetService.java
+++ src/main/java/fte/tests/jeerestcdi/rest/GreetService.java
@@ -1,11 +1,21 @@
 package fte.tests.jeerestcdi.rest;
 
-import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
-@Stateless
+import fte.tests.jeerestcdi.rest.entity.Greet;
+
 public class GreetService {
 	
+	@PersistenceContext(unitName="em1")
+	private EntityManager em;
+	
+	@Transactional
 	public String greet(String name) {
+		Greet greet = new Greet();
+		greet.setName(name);
+		em.persist(greet);
 		return "Hello "+ name;
 	}
 
